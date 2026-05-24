07397f44c7742d68475a6e8d057ac63b9e614119
kopiti74 <kopiti74@29-9>
2026-05-24 13:52:12 +0300

    Add project files.

---
+++ Practice/practice24May/pokashiki/pokashiki.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void sortingfunc(int arr[], int size, bool reverse = false) {
+	if (arr[j] > arr[j + 1]) {
+		bool reverse = false;
+	}
+}
+
+int main() {
+	
+}
